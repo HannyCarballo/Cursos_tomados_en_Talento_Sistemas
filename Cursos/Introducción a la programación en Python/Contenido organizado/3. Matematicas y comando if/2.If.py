0a7efa56efd71b0980798b57d8fcf52cf5a9166d
Hanny Carballo <launchx00194@innovaccion.mc>
2022-07-09 15:21:42 -0500

    Condicional if. Dos valores iguales

---
+++ Cursos/Introducción a la programación en Python/Contenido organizado/3. Matematicas y comando if/2.If.py
@@ -0,0 +1,18 @@
+numero_1 = 5
+numero_2 = 5
+
+if numero_1 <= numero_2:
+    print("Verdadero")
+
+else:
+    print("Falso")
+
+'''
+Respondiendo las preguntas:
+
+- ¿Que paso? 
+Se imprime en pantalla "verdadero"
+
+- ¿Por qué?
+Porque se cumple la condicional if, ya que 5 sí es igual a 5.
+'''
