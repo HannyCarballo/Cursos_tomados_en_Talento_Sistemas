d927226b07f03d7ca2b9f41386ce8da1044acbbd
Hanny Carballo <launchx00194@innovaccion.mc>
2022-07-09 01:54:05 -0500

    Ejercicio 1

---
+++ Cursos/Introducción a la programación en Python/Contenido organizado/2. Variables y conceptos básicos/1.Ejercicio1.py
@@ -0,0 +1,17 @@
+texto = "Hola mundo"
+print(texto)
+print("Esta variable es de tipo: ")
+print(type(texto))
+
+'''
+Respondiendo las preguntas:
+
+- ¿Que paso?
+Primero se imprime en pantalla el texto, el cual es hola mundo. También se imprime el tipo de variable que es.
+
+¿Por qué pasó? 
+Porque se hizo uso de las funciones print y type.
+
+¿Puedes identificar el tipo de variable en la tabla?
+Sí, String
+'''
