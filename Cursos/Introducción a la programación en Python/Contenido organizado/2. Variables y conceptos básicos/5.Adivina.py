56564d9114610c3bcf9e7eb09d766cc24750d94f
Hanny Carballo <launchx00194@innovaccion.mc>
2022-07-09 02:12:59 -0500

    Adivinar lo que pasará en el código

---
+++ Cursos/Introducción a la programación en Python/Contenido organizado/2. Variables y conceptos básicos/5.Adivina.py
@@ -0,0 +1,16 @@
+nombre_1 = 13
+nombre_2 = "Talento"
+
+nombre_3 = nombre_1 + nombre_2
+print(nombre_3)
+print("Esta variable es de tipo:")
+print(type(nombre_3))
+
+'''
+Respondiendo las preguntas:
+- ¿Pudiste predecir que pasaria? 
+No, yo creí que iba a aparecer en pantalla 13Talento y que el tipo de dato sería string.
+
+- ¿Puedes explicar porque?
+Lo que aparece en pantalla es debido a que los dos tipos de datos son distintos y no se pueden concatenar ni sumar.
+'''
