a50feef5124012dde3c5b710081dca123cca48a7
Hanny Carballo <launchx00194@innovaccion.mc>
2022-07-09 02:03:11 -0500

    Ejercicio 3

---
+++ Cursos/Introducción a la programación en Python/Contenido organizado/2. Variables y conceptos básicos/3.Ejercicio3.py
@@ -0,0 +1,20 @@
+nombre_1 = "13"
+nombre_2 = "7"
+
+nombre_3 = nombre_1 + nombre_2
+print(nombre_3)
+print("Esta variable es de tipo: ")
+print(type(nombre_3))
+
+'''
+Respondiendo las preguntas:
+
+- ¿Que paso? 
+Se concatenaron las primeras dos variables y se mostró el resultado en pantalla, así como su tipo de dato.
+
+- ¿Por qué pasó? 
+Porque como tal no se está realizando una operación, sino que se está concatenando, por eso mismo el tipo de dato es string y no entero.
+
+- ¿Que tipo de variable era?
+String.
+'''
