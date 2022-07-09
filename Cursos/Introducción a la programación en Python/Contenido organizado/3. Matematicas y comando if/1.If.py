4c86bbf014d8f00c65845a599144486293c7eb0e
Hanny Carballo <launchx00194@innovaccion.mc>
2022-07-09 15:16:49 -0500

    Condicional if

---
+++ Cursos/Introducción a la programación en Python/Contenido organizado/3. Matematicas y comando if/1.If.py
@@ -0,0 +1,25 @@
+numero_1 = 0
+numero_2 = 5
+
+numero_1 = numero_1 + 10
+if numero_1 < numero_2:
+    print("Talento sistemas")
+    print("Verdadero")
+
+else:
+    print("Falso")
+
+print(numero_1)
+
+'''
+Respondiendo las preguntas:
+
+- ¿Que imprimió? 
+Falso
+
+- ¿Por qué? 
+Porque al no cumplirse la condición del if(ya que 10 no es menor que 5), se procede a hacer lo que está en el else.
+
+- ¿Puede hacer que el programa muestre el valor de número _ 1?
+Sí, con un print.
+'''
