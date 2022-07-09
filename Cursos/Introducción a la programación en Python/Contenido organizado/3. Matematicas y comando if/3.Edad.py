5e472aa041411d284f430fc939371e487ac5b3ad
Hanny Carballo <launchx00194@innovaccion.mc>
2022-07-09 15:32:40 -0500

    Programa. Club

---
+++ Cursos/Introducción a la programación en Python/Contenido organizado/3. Matematicas y comando if/3.Edad.py
@@ -0,0 +1,8 @@
+# Programa que determina si se puede entrar a un club para mayores de edad
+edad= int(input("Ingresa tu edad, porfavor\n"))
+
+if (edad>=18):
+    print("Eres mayor de edad, puedes pasar")
+
+else:
+    print("Eres menor de edad, no puedes pasar")
