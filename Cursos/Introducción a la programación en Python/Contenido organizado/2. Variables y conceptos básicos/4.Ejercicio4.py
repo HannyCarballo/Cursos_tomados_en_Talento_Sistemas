ef4754a73045936154e9cc09097318ff328958fb
Hanny Carballo <launchx00194@innovaccion.mc>
2022-07-09 02:06:11 -0500

    Ejercicio 4

---
+++ Cursos/Introducción a la programación en Python/Contenido organizado/2. Variables y conceptos básicos/4.Ejercicio4.py
@@ -0,0 +1,17 @@
+nombre_1 = 13
+nombre_2 = 7
+
+nombre_3 = nombre_1 + nombre_2
+print(nombre_3)
+print("Esta variable es de tipo: ")
+print(type(nombre_3))
+
+'''
+Respondiendo las preguntas:
+
+- ¿Que paso? 
+Se realizó la suma de las primeras dos variables y se mostró el resultado en pantalla, así como su tipo de dato.
+
+- ¿Por qué pasó?
+Porque al quitar las comillas ya no eran caracteres sino números, y ahora en lugar de concatenar se estaba realizando una suma.
+'''
