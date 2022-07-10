e62d4b78ddd0ffaa7ca42df06e5ffaa12086c532
Hanny Carballo <launchx00194@innovaccion.mc>
2022-07-09 23:14:36 -0500

    Hello world

---
+++ Cursos/Java desde cero/Contenido organizado/1. Variables y constantes/1.HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+    public static void main(String args[]){
+        String texto = "Hello world";
+        System.out.println(texto);
+    }
+}
+
+/*
+ * Respondiendo las preguntas:
+ * ¿Que paso? 
+ * Se imprimió en pantalla el texto "Hello world"
+ * 
+ * ¿Por qué cree usted que pasó?
+ * Porque se le asigno un valor a la variable texto y con la función para imprimir en pantalla se mostró ese texto
+ */
