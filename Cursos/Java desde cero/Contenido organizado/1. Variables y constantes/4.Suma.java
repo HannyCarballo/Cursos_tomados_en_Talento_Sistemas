ccfc6ac96207022fd67602efe8f918bdb806f2a8
Hanny Carballo <launchx00194@innovaccion.mc>
2022-07-09 23:25:49 -0500

    Realizando una suma de dos variables

---
+++ Cursos/Java desde cero/Contenido organizado/1. Variables y constantes/4.Suma.java
@@ -0,0 +1,17 @@
+public class Operacion {
+    public static void main(String args[]){
+        int nombre_uno = 13;
+        int nombre_dos = 9;
+        int nombre_tres = nombre_uno + nombre_dos;
+        System.out.println(nombre_tres);
+    }
+} 
+
+/*
+ * Respondiendo las preguntas:
+ * ¿Que paso? 
+ * Se realizó una suma de dos variables
+ * 
+ * ¿Por qué pasó?
+ * Porque las variables son de tipo entero y con el signo suma se realiza la operación
+ */
