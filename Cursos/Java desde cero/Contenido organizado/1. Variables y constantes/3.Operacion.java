65ff5330079c808cd904f6d9bd1a457e0bb55586
Hanny Carballo <launchx00194@innovaccion.mc>
2022-07-09 23:22:42 -0500

    Concatenando números

---
+++ Cursos/Java desde cero/Contenido organizado/1. Variables y constantes/3.Operacion.java
@@ -0,0 +1,17 @@
+public class Operacion {
+    public static void main(String args[]){
+        String nombre_uno = "13";
+        String nombre_dos = "9";
+        String nombre_tres = nombre_uno + nombre_dos;
+        System.out.println(nombre_tres);
+    }
+} 
+
+/*
+ * Respondiendo las preguntas:
+ * ¿Que paso? 
+ * Se concatenaron los dos números
+ * 
+ * ¿Por qué pasó?
+ * Porque en realidad no se está realizando una suma, sino más bien una concatenación, ya que las variables son de tipo string
+ */
