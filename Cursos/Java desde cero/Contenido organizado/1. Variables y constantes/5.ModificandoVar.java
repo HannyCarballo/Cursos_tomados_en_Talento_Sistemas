8b04ea85cfafb2e9033b7ed390d5b9c493b75631
Hanny Carballo <launchx00194@innovaccion.mc>
2022-07-09 23:37:15 -0500

    Modificando el valor de la variable

---
+++ Cursos/Java desde cero/Contenido organizado/1. Variables y constantes/5.ModificandoVar.java
@@ -0,0 +1,8 @@
+public class Modificando {
+    public static void main(String args[]){
+        int variable = 5;
+        System.out.println(variable);
+        variable = 2;
+        System.out.println(variable);
+    }
+} 
