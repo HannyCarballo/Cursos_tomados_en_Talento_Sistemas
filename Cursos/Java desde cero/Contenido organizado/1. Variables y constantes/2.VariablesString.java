ed124bc1b123432215c892a23eec83fa1201c439
Hanny Carballo <launchx00194@innovaccion.mc>
2022-07-09 23:15:03 -0500

    Concatenando

---
+++ Cursos/Java desde cero/Contenido organizado/1. Variables y constantes/2.VariablesString.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+    public static void main(String args[]){
+        String nombre_uno = "Hello world";
+        String nombre_dos = "Escribe algo";
+        String nombre_tres = nombre_uno + nombre_dos;
+        System.out.println(nombre_tres);
+    }
+}
+
+/*
+ * Respondiendo las preguntas:
+ * ¿Qué paso? 
+ * Se concatenaron las dos variables y se mostró en pantalla
+ * 
+ * ¿Por qué cree usted que pasó?
+ * Porque se concateno con el signo de suma, por eso el texto aparece sin espacios
+ */
